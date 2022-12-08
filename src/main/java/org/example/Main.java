a44cd823d5abec97e099d66795ea814a6ce40720
iLare27 <ilare27t@gmail.com>
2022-12-08 22:03:01 +0600

    third commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 import org.json.JSONObject;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -31,8 +35,14 @@ public class Main {
     }
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner sc = new Scanner(System.in);
-        System.setProperty("webdriver.chrome.driver","selenium\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.gecko.driver","selenium\\geckodriver.exe");
+
+        FirefoxOptions options = new FirefoxOptions();
+        String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36 OPR/60.0.3255.170";
+        options.addPreference("general.useragent.override", userAgent);
+
+        WebDriver driver = new FirefoxDriver(options);
+        driver.get("https://csgorun.ru/");
 
         System.out.println("Enter a game number: ");
         int gameNum = sc.nextInt();
@@ -40,13 +50,22 @@ public class Main {
         int gameAmount = sc.nextInt();
         sc.close();
 
-        ArrayList<Double> crashList = new ArrayList<>();
-        crashList.add(99999.0);
-        crashList.add(99999.0);
-        Double crash;
-        String JsonUrl = "https://api.csgorun.ru/games/" + gameNum;
 
-        for (int i = 0; i < gameAmount; i++){
+        ArrayList<Double> crashList = new ArrayList<>();
+        Double crash;
+        String JsonUrl = "";
+
+//        for (int i = 1; i < 3; i++) {
+//            JsonUrl = "https://api.csgorun.ru/games/" + (gameNum - i);
+//            URL url_ar = new URL(JsonUrl);
+//            ReadJSON reader_ar = new ReadJSON();
+//            JSONObject json_ar = reader_ar.readJsonFromUrl(JsonUrl);  // Get 2 previous crashes
+//            JSONObject dataJson_ar = json_ar.getJSONObject("data");
+//            crashList.add(Double.valueOf(dataJson_ar.optString("crash")));
+//            Thread.sleep(1000);
+//        }
+
+        for (int i = 0; i <= gameAmount; i++) {
             JsonUrl = "https://api.csgorun.ru/games/" + (gameNum + i);
             URL url = new URL(JsonUrl);
 
@@ -58,20 +77,25 @@ public class Main {
                 crash = Double.valueOf(dataJson.optString("crash"));
                 crashList.add(crash);
 
-                System.out.println(crash);
-
-                if (crashList.get(i+2) < 1.26 && crashList.get(i+1) < 1.26 && crashList.get(i) < 1.26) {
-                    System.out.println("Launching auto-bet...");
-                    driver.get("https://csgorun.ru/");
-                }
+                System.out.println(crashList.get(i));
 
             } else {
                 i--;
-                Thread.sleep(5000);
+                Thread.sleep(2000);
             }
+
+            if (i > 1 && crashList.get(i) < 1.20 && crashList.get(i-1) < 1.20 && crashList.get(i-2) < 1.20) { // добавить условие i == (минимальное значение для проверки)
+                System.out.println("Triple crash, staking...");
+                driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[1]/div[2]/div[3]/div[2]/div/button[1]")).click(); // select item
+                Thread.sleep(1000);
+                driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[2]/div[1]/div[2]/div/div[4]/div/button")).click(); // stake
+                Thread.sleep(120000);
+            }
+
             Thread.sleep(1000);
         }
 
+        driver.quit();
     }
 }
 
