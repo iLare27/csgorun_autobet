34d5814bb5848aca3dc6de877baa177608c1deef
iLare27 <ilare27t@gmail.com>
2023-03-26 06:01:58 +0600

    cleaned code

--- src/main/java/com/ilare/CsgorunAutoBetApplication.java
+++ src/main/java/com/ilare/CsgorunAutoBetApplication.java
@@ -1,93 +1,55 @@
-package org.example;
+package com.ilare;
+
 import org.json.JSONObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxDriverLogLevel;
 import org.openqa.selenium.firefox.FirefoxOptions;
-
 import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
-public class Main {
-    public static boolean doesURLExist(URL url) throws IOException
-    {
-        // We want to check the current URL
-        HttpURLConnection.setFollowRedirects(false);
-
-        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-
-        // We don't need to get data
-        httpURLConnection.setRequestMethod("HEAD");
-
-        // Some websites don't like programmatic access so pretend to be a browser
-        httpURLConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.1.2) Gecko/20090729 Firefox/3.5.2 (.NET CLR 3.5.30729)");
-        int responseCode = httpURLConnection.getResponseCode();
-
-        // We only accept response code 200
-        return responseCode == HttpURLConnection.HTTP_OK;
-    }
+public class CsgorunAutoBetApplication {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner sc = new Scanner(System.in);
         System.setProperty("webdriver.gecko.driver","selenium\\geckodriver.exe");
-
         FirefoxOptions options = new FirefoxOptions();
         String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36";
-//        String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0";
         options.addPreference("general.useragent.override", userAgent);
-
         WebDriver driver = new FirefoxDriver(options);
         driver.get("https://csgorun.ru/");
-
         System.out.println("Enter a game number: ");
         int gameNum = sc.nextInt();
         System.out.println("Enter amount: ");
         int gameAmount = sc.nextInt();
         sc.close();
-
         ArrayList<Double> crashList = new ArrayList<>();
         Double crash;
         String JsonUrl = "";
-
         for (int i = 0; i <= gameAmount; i++) {
             JsonUrl = "https://api.csgorun.ru/games/" + (gameNum + i);
             URL url = new URL(JsonUrl);
-
-            if (doesURLExist(url)) {
-                ReadJSON reader = new ReadJSON();       // To ReadJson in order to read from url.
-                JSONObject json = reader.readJsonFromUrl(JsonUrl);  // calling method in order to read.
+            if (UtilClass.doesURLExist(url)) {
+                JSONObject json = UtilClass.readJsonFromUrl(JsonUrl);
                 JSONObject dataJson = json.getJSONObject("data");
-
                 crash = Double.valueOf(dataJson.optString("crash"));
                 crashList.add(crash);
-
                 System.out.println(crashList.get(i));
-
             } else {
                 i--;
                 Thread.sleep(2000);
             }
-
-            if (i > 1 && crashList.get(i) < 1.20 && crashList.get(i-1) < 1.20 && crashList.get(i-2) < 1.20) { // добавить условие i == (минимальное значение для проверки)
+            if (i > 1 && crashList.get(i) < 1.20 && crashList.get(i-1) < 1.20 && crashList.get(i-2) < 1.20) {
                 System.out.println("Triple crash, staking...");
                 Thread.sleep(5000);
-                driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[1]/div[2]/div[3]/div[2]/div/button[1]")).click(); // select item
+                driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[1]/div[2]/div[3]/div[2]/div/button[1]")).click();
                 Thread.sleep(1000);
-                driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[2]/div[1]/div[2]/div/div[4]/div/button")).click(); // stake
+                driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[2]/div[1]/div[2]/div/div[4]/div/button")).click();
                 Thread.sleep(120000);
             }
-
             Thread.sleep(1000);
         }
-
         driver.quit();
-
     }
 }
-
